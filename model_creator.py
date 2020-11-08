d8ee9246d19fec398ea828be3623f72566b4e7bf
S.K.Roshan <63838060+dev-Roshan-lab@users.noreply.github.com>
2020-11-08 13:02:20 +0530

    Add files via upload

---
+++ model_creator.py
@@ -0,0 +1,68 @@
+import librosa
+import numpy as np
+import joblib
+import io
+from six.moves.urllib.request import urlopen
+#Trainng
+'''
+import os
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+path = (os.path.abspath(os.path.dirname(__file__).replace("",""))+"/dataset.csv")
+dt = pd.read_csv(path)
+
+train, test = train_test_split(dt)
+
+#print(train.shape)
+#print(test.shape)
+
+train_X = train[['chroma_stft','spec_cent','spec_bw','rolloff','zcr','mfcc']]# features
+train_y = train.prognosis#labels
+
+test_X = test[['chroma_stft','spec_cent','spec_bw','rolloff','zcr','mfcc']]
+test_y = test.prognosis
+
+#knn
+model = KNeighborsClassifier(n_neighbors=3)#KNN Model for classification
+model.fit(train_X, train_y)
+
+
+#joblib.dump(model, 'covid_cough_model.sav')#save model using joblib
+
+print("model saved")
+'''
+
+audio = 'neg-cough.wav'
+
+url = "https://firebasestorage.googleapis.com/v0/b/server-65459.appspot.com/o/neg-cough.wav?alt=media&token=bd7a2d9f-b5f4-41d8-869d-9d61fd31fe17"
+#y, sr = librosa.load(io.BytesIO(urlopen(url).read()))
+#print(y.shape)
+#print(sr)
+y , sr = librosa.load(audio, mono=True, duration=1)
+#print(y.shape)
+#print(sr)
+
+chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+zcr = librosa.feature.zero_crossing_rate(y)
+mfcc = librosa.feature.mfcc(y=y, sr=sr)
+
+#loading up variables 
+chroma_stft = np.mean(chroma_stft)
+spec_cent = np.mean(spec_cent)
+spec_bw = np.mean(spec_bw)
+rolloff = np.mean(rolloff)
+zcr = np.mean(zcr)
+mfcc = np.mean(mfcc)
+
+model = joblib.load('covid_cough_model.sav')
+#print(model)
+val = np.array([chroma_stft,spec_cent,spec_bw,rolloff,zcr,mfcc])
+val=val.reshape(1,-1)
+prediction = model.predict(val)
+help(len(prediction[0]))
+
---
+++ model_trainer.py
@@ -0,0 +1,37 @@
+from csv import writer
+import os
+import librosa
+import numpy as np
+
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+        
+for count, filename in enumerate(os.listdir('pos')):
+    audio = "pos/"+filename
+    print(audio)
+    
+    y, sr = librosa.load(audio, mono=True, duration=1)
+    chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+    spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+    spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+    rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+    zcr = librosa.feature.zero_crossing_rate(y)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    
+    chroma_stft = np.mean(chroma_stft)
+    spec_cent = np.mean(spec_cent)
+    spec_bw = np.mean(spec_bw)
+    rolloff = np.mean(rolloff)
+    zcr = np.mean(zcr)
+    mfcc = np.mean(mfcc)
+    
+    contents = [chroma_stft,spec_cent,spec_bw,rolloff,zcr,mfcc,"positive"]
+    
+    append_list_as_row('dataset.csv', contents)
+    print(str(count)+ "Done")
+   
